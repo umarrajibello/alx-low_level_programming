e5d5b2ecba8a5805e23ea3035cc37aed344746ee
Raji Bello Umar <umarrajibello@gmail.com>
2022-10-15 01:56:30 +0000

    Used a puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of function
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char str[] = "Programming is like building a multilingual puzzle";
+
+	puts(str);
+	return (0);
+}
