179d288f0bfff48c882bdab6a1ef2169e07541f0
Raji Bello Umar <umarrajibello@gmail.com>
2022-10-19 15:34:29 -0700

    Added compound if files

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - This is entry point of  the main function
+*
+* Return: Always return 0 (Success)
+*/
+int main(void)
+{
+	int n;
+	int l;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	l = n % 10;
+
+	if (l > 0)
+	{
+		printf("Last string of %d is %d and is greater than 5\n", n, l);
+	}
+	else if (l == 0)
+	{
+		printf("Last string of %d is %d and is 0\n", n, l);
+	}
+	else if (l < 6 && l != 0)
+	{
+		printf("Last string of %d is %d and is less than 6 and not 0\n", n, l);
+	}
+
+
+	return (0);
+}
