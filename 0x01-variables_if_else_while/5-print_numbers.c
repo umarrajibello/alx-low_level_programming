799c317b31b5b90f4e796bbf79054d2265c59588
Raji Bello Umar <umarrajibello@gmail.com>
2022-10-21 02:32:11 -0700

    printed sigle digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry of the main function
+ *
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 0;i <= 9;i++)
+	{
+		printf("%d",i);
+	}
+	putchar('\n');
+	return (0);
+}
