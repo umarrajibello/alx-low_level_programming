0323c99366ede5d79bb22ae204915fc24c3dd743
Raji Bello Umar <umarrajibello@gmail.com>
2022-10-24 16:29:34 -0700

    Added a file to print single digit numbers of base 16 using only putchar

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints numbers of base sixteen
+ *
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	i = 0;
+	for (i = 0; i <= 9; i++)
+	{
+		putchar((i % 10) + '0');
+	}
+	c='a';
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
