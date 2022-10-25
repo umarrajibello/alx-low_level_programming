40a69172d29b3a6ad7d1d2d306037b8c37833ec6
Raji Bello Umar <umarrajibello@gmail.com>
2022-10-24 18:40:47 -0700

    created a function that updates a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_to_98 - reset value of a pointer
+ * @n: n is an int variable of the pointer in question
+ *
+ * Return: return a value of 89
+ */
+int reset_to_98(int *n)
+{
+	return (*n = 98);
+}
