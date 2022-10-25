2683b4ab0c2a2c002ef787a7f8b5087e213063b7
Raji Bello Umar <umarrajibello@gmail.com>
2022-10-24 17:38:36 -0700

    write a program that prints putchar without using stdio.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - This program prints puchchar
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar('_'), _putchar('p'), _putchar('u'), _putchar('t');
+	_putchar('c'); _putchar('h'), _putchar('a'), _putchar('r');
+	_putchar('\n');
+	return (0);
+}
