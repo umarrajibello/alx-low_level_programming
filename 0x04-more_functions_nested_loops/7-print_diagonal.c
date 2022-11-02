b0460c52c11c49974040442160eab55b703775ac
Raji Bello Umar <umarrajibello@gmail.com>
2022-11-02 07:30:36 -0700

    codded to print diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - program to draw a diagonal
+ * @n: is the number that determine the length of the line
+ */
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int i, j;
+
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 2; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
