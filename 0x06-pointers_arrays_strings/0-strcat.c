42c9afe5abda3c4506da91f3ce1cf2f63b1f0190
Raji Bello Umar <umarrajibello@gmail.com>
2022-11-07 05:42:52 -0800

    written an strcat function

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,22 @@
 #include <string.h>
 /**
  * *_strcat - concatenates two strings
- * @*est: the first string in question
- * @*src: the second string in question
+ * @dest: the first string in question
+ * @src: the second string in question
  * Return: return a bigger string after concatenation
  */
 char *_strcat(char *dest, char *src)
 {
 	size_t i, j;
 
-	for(i = 0; dest[i] != '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++)
 	{
 		;
 	}
-	for(j = 0; src[j] != '\0'; j++)
+	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[j + i] = src[j];
 	}
 	dest[i + j] = '\0';
-	return dest;
+	return (dest);
 }
