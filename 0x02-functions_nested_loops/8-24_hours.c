c83aeb8511bc39499a03f1c2c293a77c9e169c48
Raji Bello Umar <umarrajibello@gmail.com>
2022-11-12 23:17:56 -0800

    written code to print 24 hrs

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * jack_bauer - program to print 24 hours
@@ -17,12 +18,7 @@ void jack_bauer(void)
 			{
 				for (l = 0; l <= 9; l++)
 				{
-					_putchar('0' + i);
-					_putchar('0' + j);
-					_putchar(':');
-					_putchar('0' + k);
-					_putchar('0' + l);
-					_putchar('\n');
+					printf("%d%d:%d%d\n", i, j, k, l);
 				}
 			}
 		}
