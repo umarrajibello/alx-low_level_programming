6c552afb995ff5c6658d10c19e3369834fccbecf
Raji Bello Umar <umarrajibello@gmail.com>
2022-12-02 03:42:27 -0800

    codded to display the name the program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - argc and  argv
+ * main - program to use environment variables argc and  argv.
+ *@argc: the cout of environment variables.
+ *@argc: an array of names of input variables.
  *
  * Return: Always 0.
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc-1]);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
