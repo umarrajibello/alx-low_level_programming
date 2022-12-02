1aca9465ef4d9d2daa0ff6826b0a87090f85e45a
Raji Bello Umar <umarrajibello@gmail.com>
2022-12-02 04:11:17 -0800

    codded to display the name of the program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,12 @@
 /**
  * main - program to use environment variables argc and  argv.
  *@argc: the cout of environment variables.
- *@argc: an array of names of input variables.
+ *@argv: an array of names of input variables.
  *
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	printf("%s\n", argv[argc - 1]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
