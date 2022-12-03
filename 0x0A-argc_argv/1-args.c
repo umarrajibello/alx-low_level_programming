5542d1a75a7a761dbaa75eb9d4537356807874a5
Raji Bello Umar <umarrajibello@gmail.com>
2022-12-02 23:11:10 -0800

    codded to print number of input args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program to use environment variables argc and  argv.
+ *@argc: the cout of environment variables.
+ *@argv: an array of names of input variables.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
