498c777a4223378a94aa34553d2b171815b6cf54
Raji Bello Umara <umarrajibello@gmail.com>
2023-05-19 03:28:22 -0700

    Written to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (success)
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str, sizeof(str));
+	return (1);
+}
