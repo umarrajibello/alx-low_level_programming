70a1de82874f554aeb6a2f01b7c06e0fe27f3908
Raji Bello Umara <umarrajibello@gmail.com>
2023-05-19 14:23:25 -0700

    Printed combination of 2 and 3 digit numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,20 +7,24 @@
  */
 int main(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = i + 1; j <= 9; j++)
 		{
-			putchar('0' + i);
-			putchar('0' + j);
-			if (i >= 8 && j >= 9)
+			for (k = j + 1; k <= 9; k++)
 			{
-				break;
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
+				if (i >= 7 && j >= 8 && k >= 9)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
 			}
-			putchar(',');
-			putchar(' ');
 		}
 	}
 	putchar('\n');
