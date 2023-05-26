fceee85696b9cddd8ac92778bde5055b5d747c91
Raji Bello Umara <umarrajibello@gmail.com>
2023-05-25 21:57:36 -0700

    Codded to multipy

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 numbers
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: returns the product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdio.h>
+
 int _isupper(int);
 int _isdigit(int);
-#endif
+int mul(int, int);
+#endif /* MAIN_H */
