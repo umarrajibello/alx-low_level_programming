a0a2da9b863c70a6b3d14844c557b67452530d11
Raji Bello Umara <umarrajibello@gmail.com>
2023-05-30 22:01:20 -0700

    Reversing a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - prints a str in rev
+ * @str: the str to be printed
+ *
+ * Return: returns void.
+ */
+void rev_string(char *str)
+{
+	int len, i, tmp;
+
+	len = strlen(str);
+	for (i = 0; i < len/2 ; i++)
+	{
+		tmp = *(str + i);
+		*(str + i) = *(str + len - i - 1);
+		*(str + len - i - 1) = tmp;
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ void swap_int(int *, int *);
 int _strlen(char *);
 void _puts(char *);
 void print_rev(char *);
+void rev_string(char *);
 
 #endif /* MAIN_H */
