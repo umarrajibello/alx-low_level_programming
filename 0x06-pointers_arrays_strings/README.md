256f89a1a382f302bafef57fe0e133ee184695fa
Raji Bello Umara <umarrajibello@gmail.com>
2023-05-31 13:29:08 -0700

    Codded to concartnate strings

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a directory for more pointers arrays and strings
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+
+char *_strcat(char *, char *);
+
+#endif /* MAIN_H */
