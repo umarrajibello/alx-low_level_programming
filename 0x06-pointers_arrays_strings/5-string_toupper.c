fc49560a7009508945a1081a8649eb2a5168adff
Raji Bello Umara <umarrajibello@gmail.com>
2023-06-02 17:51:24 -0700

    Codded to covert lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts lower case to upper case
+ * @str: the string to check and convert
+ *
+ * Return: return the resultant string in upper case.
+ */
+char *string_toupper(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		if (*str >= 97 && *str <= 122)
+		{
+			*str = *str - 96 + 64;
+		}
+	}
+	return (str);
+}
