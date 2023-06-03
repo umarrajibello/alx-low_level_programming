61b2652c44c311a3ca44a6de727ac20550c499e6
Raji Bello Umara <umarrajibello@gmail.com>
2023-06-02 17:31:21 -0700

    Codded to reverse an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,10 +11,9 @@ void reverse_array(int *a, int n)
 {
 	int i, tmp;
 
-	tmp = 0;
 	for (i = 0; i < n / 2; i++)
 	{
-		*(a + i) = tmp;
+		tmp = *(a + i);
 		*(a + i) = *(a + n - 1 - i);
 		*(a + n - 1 - i) = tmp;
 	}
