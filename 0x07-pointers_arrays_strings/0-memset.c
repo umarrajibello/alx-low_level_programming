2b3179099799bf708c98dd13c18447bb1245d8a0
Raji Bello Umara <umarrajibello@gmail.com>
2023-06-05 14:53:35 -0700

    Codded to fill memory with constant bytes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,15 +2,16 @@
 
 /**
  * _memset - Copy constant bytes in memory
- * @
- * @
- * @
+ * @s: the pointer to the memory location
+ * @b: the constant byte
+ * @n: the number of bytes to be filled in
  *
  * Return: returns
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
