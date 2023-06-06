195f8106f1b9c0a77bbfce5a284bf862eff5ce93
Raji Bello Umara <umarrajibello@gmail.com>
2023-06-05 18:07:10 -0700

    Codded to add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: the sum of the numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,4 +14,5 @@ int add(int, int);
 void print_to_98(int);
 void print_times_table(int);
 
+
 #endif
