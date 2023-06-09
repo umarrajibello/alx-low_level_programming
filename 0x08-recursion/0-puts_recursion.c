12263aa00d0d7f14916cc375e10fa4b26c87488f
Raji Bello Umara <umarrajibello@gmail.com>
2023-06-08 21:42:17 -0700

    Codded puts with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,9 +8,14 @@
  */
 void _puts_recursion(char *s)
 {
-	for (; *s != '\0'; s++)
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
 	{
 		_putchar(*s);
+		s++;
+		_puts_recursion(s);
 	}
-	_putchar('\n');
 }
