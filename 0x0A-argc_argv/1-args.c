c5d63cd05198cfa1051aea5011f7827b792d85db
Raji Bello Umara <umarrajibello@gmail.com>
2023-06-09 23:06:45 -0700

    Codded to count num of command line args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: number of command line args
+ * @argv: string of command line args
+ *
+ * Return: returns 0.
+ */
+int main(int argc, __attribute__((unused)) char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
