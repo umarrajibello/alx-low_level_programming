c28e8926945afcfd86784751ee50327c47db1021
Raji Bello Umara <umarrajibello@gmail.com>
2023-06-15 14:47:53 -0700

    Codded to duplicate a string and return location

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strdup - creates a duplicate of a string
+ * @str: the pointer to the string in question
+ *
+ * Return: returns a pointer to duplicate string
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	unsigned int i, s_len;
+
+	s_len = strlen(str);
+	s = malloc(sizeof(str));
+	if ((str == NULL) || (s == NULL))
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= s_len; i++)
+	{
+		s[i] = str[i];
+	}
+	return (s);
+}
