5d79e560008d5dac6ffa095befb9303a7309f944
Raji Bello Umara <umarrajibello@gmail.com>
2023-06-17 04:20:36 -0700

    Codded to concartnate two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - concatnates two strings
+ * @s1: the pointer to the 1st string
+ * @s2: the pointer to the 2nd string
+ *
+ * Return: returns a pointer to the concartnated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	unsigned int i, s_len, s1_len, s2_len;
+
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
+	s_len = s1_len + s2_len;
+	s = malloc(s_len + 1);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < s1_len; i++)
+	{
+		s[i] = s1[i];
+	}
+	for (i = s1_len; i < s_len; i++)
+	{
+		s[i] = s2[i - s1_len];
+	}
+	s[s_len] = '\0';
+	return (s);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,6 +8,7 @@
 int _putchar(char);
 char *create_array(unsigned int, char);
 char *_strdup(char *);
+char *str_concat(char *, char *);
 
 
 #endif
