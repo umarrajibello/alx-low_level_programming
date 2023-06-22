6bcb7866ab252c8d30f1ccf2d1d51cd14712ee9c
Raji Bello Umar <umarrajibello@gmail.com>
2023-06-22 22:24:44 +0000

    Codded to print name using function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name using function pointers
+ * @name: name to be printed
+ * @f: the function pointer
+ *
+ * Return: returns nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char);
+void print_name(char *name, void (*f)(char *));
+
+
+#endif /* MAIN_H */
