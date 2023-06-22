2047f105d002ac8f599dfa6f01df46c17e153a46
Raji Bello Umar <umarrajibello@gmail.com>
2023-06-22 11:53:28 +0000

    Used variadic functions to sum inputs

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums all its input.
+ * @n: number of variable args
+ *
+ * Return: returns the sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int x, sum;
+	va_list param;
+
+	va_start(param, n);
+	sum = 0;
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(param, int);
+		sum += x;
+	}
+	va_end(param);
+	return (sum);
+}
+
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTTIONS_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <strings.h>
+int _putchar(char);
+int sum_them_all(const unsigned int, ...);
+#endif /* VARIADIC_FUNCTIONS_H */
