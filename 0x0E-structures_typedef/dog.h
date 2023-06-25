08b48faf3bd673854c86022e345d0a434ddf8117
Raji Bello Umar <umarrajibello@gmail.com>
2023-06-25 14:08:39 +0000

    Codded to create a struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - a struct with three members
+ * @name: a string storing name
+ * @age: a float storing age
+ * @owner: the owner of the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*_DOG_H */
