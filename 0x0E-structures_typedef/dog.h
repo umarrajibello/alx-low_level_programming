33c97f2b273429c2446b0154623d3c4813473d4a
Raji Bello Umar <umarrajibello@gmail.com>
2023-06-26 00:12:10 +0000

    Codded to declare a new type struct variable

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,9 +12,9 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+typedef struct dog dog_t;
 #endif /*_DOG_H */
