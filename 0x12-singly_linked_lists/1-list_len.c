e1fd517f5f03d2f1908bc5f4465b750f9b65d6f8
Raji Bello Umar <umarrajibello@gmail.com>
2023-07-05 04:53:24 +0000

    Codded to count the no. of elements in a sll

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,28 +1,23 @@
 #include "lists.h"
 
 /**
- * print_list - prints the all the nodes of a singly linked list
+ * list_len - counts the number of nodes of a singly linked list
  * @h: the pointer to the head
  *
  * Return: the number of nodes printed
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t count = 0;
 	const list_t *ptr;
 
 	if (h == NULL)
-	{
-		printf("(nil)\n");
 		return (count);
-	}
 	ptr = h;
 	while (ptr != NULL)
 	{
 		if (ptr->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%lu] %s\n", strlen(ptr->str), ptr->str);
+			return (count);
 		count++;
 		ptr = ptr->next;
 	}
