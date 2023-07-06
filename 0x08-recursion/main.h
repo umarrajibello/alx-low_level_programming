5764e071262b085f180925d11ca5c7bc39ec6315
Umar Raji Bello <umarrajibello@gmail.com>
2023-07-06 13:18:17 +0000

    Calculate power using recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,7 @@ void _putchar(char);
 void _print_rev_recursion(char *);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _pow_recursion(int x, int y);
 void _puts_recursion(char *);
 
 #endif /* MAIN_H */
