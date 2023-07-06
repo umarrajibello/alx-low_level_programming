da7d8688c1c8fed6f6ba5baa23315d6b50f43eb4
Umar Raji Bello <umarrajibello@gmail.com>
2023-07-06 01:33:13 +0000

    Updated 5-string_to_upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts lower case to upper case
+ * @str: the string to check and convert
+ *
+ * Return: return the resultant string in upper case.
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+	
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}
