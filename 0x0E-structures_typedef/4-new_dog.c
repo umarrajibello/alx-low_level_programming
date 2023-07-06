241396489fbe962fe1d2ef375f8277b84ae0e8dc
Umar Raji Bello <umarrajibello@gmail.com>
2023-07-06 10:28:27 +0000

    Created new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - struct type dog_t
@@ -13,12 +14,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *p;
 
-	p = malloc(sizeof (dog_t));
+	p = malloc(sizeof(dog_t));
 	if (p == NULL)
 		return (NULL);
 
-	p->name = name;
+	p->name = strcpy(name);
 	p->age = age;
-	p->owner = owner;
+	p->owner = strcp(owner);
 	return (p);
 }
