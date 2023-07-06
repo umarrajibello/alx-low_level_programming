1d24f146fd9f8db32a5a0268ff4bf35ef9cc1120
Umar Raji Bello <umarrajibello@gmail.com>
2023-07-06 13:57:41 +0000

    Codded malloc-checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: character to fill in the array
+ *
+ * Return: returns a pointer to the array
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *buff;
+
+	buff = malloc(b * sizeof(unsigned int));
+	if ((b <= 0) || (buff == NULL))
+	{
+		exit (98);
+	}
+	return (buff);
+}
