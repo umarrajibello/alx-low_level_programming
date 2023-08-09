dbc5456a2f876cfd0c8d541922e1754e5149c0d7
Umar Raji Bello <umarrajibello@gmail.com>
2023-08-09 23:33:52 +0000

    Malloc'ed to concartnate 2 strs

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,29 +16,18 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, s_len, s1_len, s2_len;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	s1_len = strlen(s1);
 	s2_len = strlen(s2);
 	s_len = s1_len + s2_len;
 	s = malloc(s_len + 1);
 	if (s == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; i < s1_len; i++)
-	{
 		s[i] = s1[i];
-	}
-	for (i = s1_len; i < s_len; i++)
-	{
+	for (; i <= s_len; i++)
 		s[i] = s2[i - s1_len];
-	}
-	s[s_len] = '\0';
 	return (s);
 }
