4c6297616a635bfea9fe880ea5eb991a60b2b4ff
Umar Raji Bello <umarrajibello@gmail.com>
2023-08-09 21:42:12 +0000

    Malloc'ed an array initiallized with specific char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,15 +12,10 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
-	arr = malloc(sizeof(size) + 1);
+	arr = malloc(size * sizeof(unsigned int));
 	if ((size == 0) || (arr == NULL))
-	{
 		return (NULL);
-	}
 	for (i = 0; i < size; i++)
-	{
 		arr[i] = c;
-	}
-	arr[i] = '\0'
 	return (arr);
 }
