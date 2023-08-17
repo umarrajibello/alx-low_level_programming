72f2bb76a92c527946754290850275a3ab403b02
Umar Raji Bello <umarrajibello@gmail.com>
2023-08-17 08:41:15 +0000

    Codded variadic functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums all its input.
+ * @n: number of variable args
+ *
+ * Return: returns the sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum;
+	va_list param;
+
+	if (n == 0)
+		return (0);
+
+	va_start(param, n);
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(param, int);
+
+	va_end(param);
+	return (sum);
+}
+
