89bf7c32700f5b5ab1baa98563753117b8de82f6
Umar Raji Bello <umarrajibello@gmail.com>
2023-08-19 08:56:57 +0000

    Codded Task 3 function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,7 +20,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
-		if (*(ops[i].op) == *s)
+		if ((*(ops[i].op) == *s) && (*(s + 1) == '\0'))
 			return (ops[i].f);
 		i++;
 	}
