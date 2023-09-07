0b7928782aff79cfaa12443d92c3b42630706363
Umar Raji Bello <umarrajibello@gmail.com>
2023-09-07 01:45:25 +0000

    Ensured main.h conforms with betty style

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_binary - prints a number in binary form
+ * @n: the number
+ *
+ * Return: returns nothing
+ */
+void print_binary(unsigned long int n)
+{
+	n << 2;
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
-#define MAIN_H 
+#define MAIN_H
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif /* MAIN_H */
