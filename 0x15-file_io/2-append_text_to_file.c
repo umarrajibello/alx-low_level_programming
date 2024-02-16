3d6295a32b43620c99123e133fa1b2624490490c
Umar Raji Bello <umarrajibello@gmail.com>
2024-02-17 03:34:36 +0800

    Tasks for the file i/o completed

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,7 +4,7 @@
 /**
  * _strlen - counts string length
  * @str: string to be used
- * 
+ *
  * Return: length of the string
  */
 int _strlen(char *str)
@@ -20,7 +20,7 @@ int _strlen(char *str)
  * append_text_to_file - appends text at the end of a file
  * @filename: name of the file
  * @text_content: content to be appended
- * 
+ *
  * Return: 1 on success, -1 otherwise
  */
 int append_text_to_file(const char *filename, char *text_content)
